bfe3bcca500ca77c0a501868977f36ada6ab0eac
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2026-07-10 23:29:08 +0100

    completed mobile and tablet view

--- profile.js
+++ profile.js
@@ -1,8 +1,8 @@
 document.getElementById('profile-form')?.addEventListener('submit', async (event) => {
-  ...
+  
   if (result.success) {
     // alert('profile submited successfully');
-    setFeedback('profile-feedback', ...);
+    setFeedback('profile-feedback', );
     document.getElementById('profile-contact-email').textContent = data.email;
     document.getElementById('profile-contact-phone').textContent = data.phone;
   }
